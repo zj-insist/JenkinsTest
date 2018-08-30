4bd67e8cc378c1bb67e976ccc4bb55c5854ed698
Jie <373872469@qq.com>
2018-08-30 11:16:38 +0800

    🚧 test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Ezzzzzzzzzzzzzzzz"/>
+    <HelloWorld msg="Hello World!"/>
   </div>
 </template>
 
